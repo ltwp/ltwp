1219c0fb2ce738d9c66ca59f486e1644c9924531
Lukas <lukaswinklerprins@gmail.com>
2016-01-16 19:44:13 -0500

    Instated top partials, CSS and JS, beginning tempting language.

---
+++ public/docs/js/link_color.js
@@ -0,0 +1,23 @@
+$("a").hover(function(){
+  console.log("Link hover; changing color.");
+  //var randomColor = Math.floor(Math.random()*16777215).toString(16);
+  var randomColor = "hsl("+Math.floor(Math.random()*360).toString()+",75%,55%)";
+  $(this).css("color",""+randomColor);
+  //$(this).css("border-bottom","2px solid #"+randomColor);
+  $(this).css("border-bottom","2px solid "+randomColor);
+  $('#top').css("background-color",""+randomColor);
+  $('#top').css("visibility","visible");
+  $('#left').css("background-color",""+randomColor);
+  $('#left').css("visibility","visible");
+  $('#right').css("background-color",""+randomColor);
+  $('#right').css("visibility","visible");
+  $('#bottom').css("background-color",""+randomColor);
+  $('#bottom').css("visibility","visible");
+},function(){
+  $(this).css("color","black");
+  $(this).css("border-bottom","2px solid black");
+  $('#top').css("visibility","hidden");
+  $('#left').css("visibility","hidden");
+  $('#right').css("visibility","hidden");
+  $('#bottom').css("visibility","hidden");
+});
